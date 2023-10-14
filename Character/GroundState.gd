1b821ce958cd437f8d6b278f6102c10b06de1bc1
Leonardo Schumann <leo_cani@hotmail.com>
2023-10-14 14:25:13 -0300

    crouch mechanic added
    Added the crouchWalk mechanic to the ground state, plus correcting the running bug

--- Character/GroundState.gd
+++ Character/GroundState.gd
@@ -8,9 +8,13 @@ class_name GroundState
 @onready var state_machine : CharacterStateMachine = $/root/TestLevel/player2/CharacterStateMachine
 
 func _physics_process(delta):
-		if character.direction.x != 0 && state_machine.check_if_can_move():
+		if character.direction.x != 0 && state_machine.check_if_can_move() && state_machine.current_state != running_state:
 		#Walk Mechanics
 			character.velocity.x = character.direction.x * character.WalkSpeed
+			if Input.is_action_pressed("crouch"):
+				character.velocity.x = character.direction.x * character.CrouchSpeed
+		else:
+			character.velocity.x = move_toward(character.velocity.x, 0, character.WalkSpeed)
 
 
 func state_input(event : InputEvent):
